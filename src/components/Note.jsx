5eb55165907bcb8541aa60ba5352a4a0230cd448
Marinela Togoe <marinelatogoe@gmail.com>
2023-06-07 22:22:42 +0200

    Add the components folder with the Header, Note and Footer files

---
+++ src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const Note = () => {
+
+    return(
+        <div className="note">
+            <h1>The Title...</h1>
+            <p>Your Content...</p>
+        </div>
+    );
+}
+
+export default Note;
