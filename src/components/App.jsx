b2654f14bf7ada8484f48b7bf5bad9fba2c6c9de
Marinela Togoe <marinelatogoe@gmail.com>
2023-06-13 19:20:34 +0200

    add the createNotes() function inside App.jsx outside the App() function scope

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,21 @@ import Footer from './Footer';
 import Note from './Note';
 import notes from '../notes';
 
-console.log(notes);
+
+
+function createNotes(noteItem) {
+    return <Note 
+        key={noteItem.key}
+        title={noteItem.title}
+        content={noteItem.content}
+    />
+}
 
 function App () {
     return(
      <div>
         <Header />
-        <Note 
-            title="The Title..."
-            content="Your Content..."
-        />
+        {notes.map(createNotes)}
         <Footer />
      </div>  
     );
